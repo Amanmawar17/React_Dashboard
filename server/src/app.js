2dcd37c53f8e09ac5ee0ca78689e2a3ba820d04d
Frieden <amanmawar17141@gmail.com>
2024-07-27 12:26:47 +0530

    whitelisting

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,19 @@ import express from "express"
 import cors from "cors"
 
 const app = express()
-
+var whitelist = ['https://react-dashboard-q8hw.vercel.app', 'http://localhost:5173']
+var corsoption = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  }
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
     credentials: true
-}))
+}, corsoption))
 
 app.use(express.json({limit: "900kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
